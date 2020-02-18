8d87547aa78876b5ab408e9d410e45efaf7e2921
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-02-18 22:45:04 +0100

    Add test case of requires_visit_to_world=True

--- test/resolver/test_resolver_reach.py
+++ test/resolver/test_resolver_reach.py
@@ -1,7 +1,20 @@
 from unittest.mock import MagicMock, PropertyMock
 
-from randovania.game_description.node import EventNode
+import pytest
+
+from randovania.game_description.area import Area
+from randovania.game_description.area_location import AreaLocation
+from randovania.game_description.game_description import GameDescription
+from randovania.game_description.game_patches import GamePatches
+from randovania.game_description.node import EventNode, GenericNode, TeleporterNode
+from randovania.game_description.requirements import RequirementSet, RequirementList, IndividualRequirement
+from randovania.game_description.resources.pickup_index import PickupIndex
+from randovania.game_description.world import World
+from randovania.game_description.world_list import WorldList
+from randovania.resolver import bootstrap
+from randovania.resolver.logic import Logic
 from randovania.resolver.resolver_reach import ResolverReach
+from randovania.resolver.state import State
 
 
 def test_possible_actions_empty():
@@ -51,3 +64,66 @@ def test_possible_actions_with_event():
     event.can_collect.assert_called_once_with(state.patches, state.resources)
     logic.get_additional_requirements.assert_called_once_with(event)
     logic.get_additional_requirements.return_value.satisfied.assert_called_once_with(state.resources, 1)
+
+
+@pytest.fixture(name="visit_restriction_logic")
+def _visit_restriction_logic(default_layout_configuration, echoes_game_description):
+    node_1 = GenericNode("Start", False, 0)
+    node_y = GenericNode("Y", False, 1)
+    node_z = GenericNode("Z", False, 2)
+    node_x_to_y = TeleporterNode("ToY", False, 3, 10, AreaLocation(2000, 6000), False, False, False)
+    node_x_to_z = TeleporterNode("ToZ", False, 4, 11, AreaLocation(2000, 7000), False, False, True)
+
+    area_x = Area("X", False, 5000, 0, [node_1, node_x_to_y, node_x_to_z], {
+        node_1: {
+            node_x_to_y: RequirementSet([RequirementList(0, [
+                IndividualRequirement(PickupIndex(0), 1, False),
+            ])]),
+            node_x_to_z: RequirementSet.trivial(),
+        },
+        node_x_to_y: {},
+        node_x_to_z: {},
+
+    })
+    area_y = Area("Y", False, 6000, 0, [node_y], {node_y: {}})
+    area_z = Area("Z", False, 7000, 0, [node_z], {node_z: {}})
+
+    worlds = [
+        World("A", "A", 1000, [area_x]),
+        World("B", "B", 2000, [area_y, area_z]),
+    ]
+
+    game = GameDescription(
+        game=echoes_game_description.game,
+        game_name=echoes_game_description.game_name,
+        dock_weakness_database=echoes_game_description.dock_weakness_database,
+
+        resource_database=echoes_game_description.resource_database,
+        victory_condition=RequirementSet.impossible(),
+        starting_location=AreaLocation(1000, 5000),
+        initial_states={"Default": ()},
+        world_list=WorldList(worlds),
+    )
+    return Logic(
+        game=game,
+        configuration=default_layout_configuration,
+    )
+
+
+@pytest.mark.parametrize("is_reachable", [False, True])
+def test_teleporter_with_visit_restriction_success(visit_restriction_logic, is_reachable):
+    # Setup
+    initial_state = bootstrap.calculate_starting_state(visit_restriction_logic.game,
+                                                       GamePatches.with_game(visit_restriction_logic.game))
+    initial_state.resources[PickupIndex(0)] = 1 if is_reachable else 0
+    target_area = visit_restriction_logic.game.world_list.area_by_area_location(AreaLocation(2000, 7000))
+    target_node = target_area.nodes[0]
+
+    # Run
+    reach = ResolverReach.calculate_reach(visit_restriction_logic, initial_state)
+
+    # Assert
+    if is_reachable:
+        assert target_node in reach.nodes
+    else:
+        assert target_node not in reach.nodes
