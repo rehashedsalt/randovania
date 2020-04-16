56d39320023eafdeb748dd9c4468c983df02ffbd
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-04-16 23:01:35 +0200

    Load positions

--- randovania/gui/dialog/area_location_dialog.py
+++ randovania/gui/dialog/area_location_dialog.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from PySide2.QtCore import QRect, QPoint, QSize
 from PySide2.QtGui import QPixmap, QMouseEvent
-from PySide2.QtWidgets import QMainWindow, QLabel, QWidget, QRadioButton, QPushButton, QDialog
+from PySide2.QtWidgets import QMainWindow, QLabel, QWidget, QRadioButton, QPushButton, QDialog, QFileDialog, QMessageBox
 
 from randovania.game_description.area import Area
 
@@ -51,6 +51,11 @@ class AreaLocationDialog(QDialog):
         save_button.clicked.connect(self._save_positions)
         save_button.setGeometry(QRect(QPoint(0, pixmap.height()), save_button.size()))
 
+        load_button = QPushButton(central_widget)
+        load_button.setText("Load Positions")
+        load_button.clicked.connect(self._load_positions)
+        load_button.setGeometry(QRect(QPoint(save_button.width() + 10, save_button.y()), load_button.size()))
+
         central_widget.setMinimumSize(pixmap.width(), pixmap.height() + 30)
         self.resize(central_widget.size())
 
@@ -66,3 +71,27 @@ class AreaLocationDialog(QDialog):
                     for radio in self.radios
                 ]
             }, f)
+
+    def _load_positions(self):
+        open_result = QFileDialog.getOpenFileName(self, caption="Select previously exported image sizes.",
+                                                  filter="*.positions.json")
+        if not open_result or open_result == ("", ""):
+            return
+
+        with open(open_result[0]) as file_to_load:
+            loaded_data = json.load(file_to_load)
+
+        if len(loaded_data["positions"]) != len(self.radios):
+            QMessageBox.critical(self,
+                                 "Invalid positions file",
+                                 ("Provided positions file has {} positions instead of {}. "
+                                  "It's also for area {}, while {} is being edited.").format(
+                                     len(loaded_data["positions"]),
+                                     len(self.radios),
+                                     loaded_data["area_name"],
+                                     self.area.name
+                                 ))
+            return
+
+        for new_position, radio in zip(loaded_data["positions"], self.radios):
+            radio.move(new_position["x"], new_position["y"])
